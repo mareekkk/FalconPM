2460a25cfaec8eded88c199bc4f4e64fdd3df971
marek <marek@v2202506285203358227.hotsrv.de>
2025-09-26 19:14:05 +0200

    Hunter system initialized without error / no tasks yet.

---
+++ build/CMakeFiles/falconpm_base.dir/src/AI/lanner/lanner.cpp.o.d
@@ -0,0 +1,22 @@
+CMakeFiles/falconpm_base.dir/src/AI/lanner/lanner.cpp.o: \
+ /home/marek/FalconPM/src/AI/lanner/lanner.cpp /usr/include/stdc-predef.h \
+ /home/marek/FalconPM/src/AI/lanner/lnr_state.h \
+ /home/marek/FalconPM/src/AI/lanner/lnr_api.h \
+ /home/marek/FalconPM/src/AI/lanner/../../infra/plugin_api.h \
+ /usr/lib/gcc/aarch64-linux-gnu/12/include/stdint.h /usr/include/stdint.h \
+ /usr/include/aarch64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/aarch64-linux-gnu/bits/wordsize.h \
+ /usr/include/aarch64-linux-gnu/bits/timesize.h \
+ /usr/include/aarch64-linux-gnu/sys/cdefs.h \
+ /usr/include/aarch64-linux-gnu/bits/long-double.h \
+ /usr/include/aarch64-linux-gnu/gnu/stubs.h \
+ /usr/include/aarch64-linux-gnu/gnu/stubs-lp64.h \
+ /usr/include/aarch64-linux-gnu/bits/types.h \
+ /usr/include/aarch64-linux-gnu/bits/typesizes.h \
+ /usr/include/aarch64-linux-gnu/bits/time64.h \
+ /usr/include/aarch64-linux-gnu/bits/wchar.h \
+ /usr/include/aarch64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/aarch64-linux-gnu/bits/stdint-uintn.h \
+ /usr/lib/gcc/aarch64-linux-gnu/12/include/stddef.h \
+ /usr/lib/gcc/aarch64-linux-gnu/12/include/stdbool.h
---
+++ build/CMakeFiles/falconpm_base.dir/src/AI/merlin/mln_attack.cpp.o.d
@@ -0,0 +1,45 @@
+CMakeFiles/falconpm_base.dir/src/AI/merlin/mln_attack.cpp.o: \
+ /home/marek/FalconPM/src/AI/merlin/mln_attack.cpp \
+ /usr/include/stdc-predef.h \
+ /home/marek/FalconPM/src/AI/merlin/mln_attack.h \
+ /home/marek/FalconPM/src/AI/merlin/../../infra/plugin_api.h \
+ /usr/lib/gcc/aarch64-linux-gnu/12/include/stdint.h /usr/include/stdint.h \
+ /usr/include/aarch64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/aarch64-linux-gnu/bits/wordsize.h \
+ /usr/include/aarch64-linux-gnu/bits/timesize.h \
+ /usr/include/aarch64-linux-gnu/sys/cdefs.h \
+ /usr/include/aarch64-linux-gnu/bits/long-double.h \
+ /usr/include/aarch64-linux-gnu/gnu/stubs.h \
+ /usr/include/aarch64-linux-gnu/gnu/stubs-lp64.h \
+ /usr/include/aarch64-linux-gnu/bits/types.h \
+ /usr/include/aarch64-linux-gnu/bits/typesizes.h \
+ /usr/include/aarch64-linux-gnu/bits/time64.h \
+ /usr/include/aarch64-linux-gnu/bits/wchar.h \
+ /usr/include/aarch64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/aarch64-linux-gnu/bits/stdint-uintn.h \
+ /usr/lib/gcc/aarch64-linux-gnu/12/include/stddef.h \
+ /usr/lib/gcc/aarch64-linux-gnu/12/include/stdbool.h \
+ /home/marek/FalconPM/src/AI/merlin/../../core/falconpm.hpp \
+ /usr/include/c++/12/cstdio \
+ /usr/include/aarch64-linux-gnu/c++/12/bits/c++config.h \
+ /usr/include/aarch64-linux-gnu/c++/12/bits/os_defines.h \
+ /usr/include/aarch64-linux-gnu/c++/12/bits/cpu_defines.h \
+ /usr/include/c++/12/pstl/pstl_config.h /usr/include/stdio.h \
+ /usr/lib/gcc/aarch64-linux-gnu/12/include/stdarg.h \
+ /usr/include/aarch64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/aarch64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/aarch64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/aarch64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/aarch64-linux-gnu/bits/types/FILE.h \
+ /usr/include/aarch64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/aarch64-linux-gnu/bits/types/cookie_io_functions_t.h \
+ /usr/include/aarch64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/aarch64-linux-gnu/bits/floatn.h \
+ /usr/include/aarch64-linux-gnu/bits/floatn-common.h \
+ /usr/include/c++/12/cstdint \
+ /home/marek/FalconPM/src/AI/merlin/../peregrine/pgn_api.h \
+ /home/marek/FalconPM/src/AI/merlin/../peregrine/pgn_gat.h \
+ /home/marek/FalconPM/src/AI/merlin/../peregrine/pgn_path.h \
+ /home/marek/FalconPM/src/AI/merlin/../lanner/lnr_api.h \
+ /home/marek/FalconPM/src/AI/merlin/../lanner/lnr_state.h
